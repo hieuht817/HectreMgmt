b051278143657b4f3e38a9b5ac23aaf7f5e7b079
AlanHoang <alan.hoang@hxro.io>
2022-01-14 17:30:44 +0700

    seeding data

---
+++ WebApplication1/Persistence/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using HectreMgmt.Domain.Model;
+using System.Text;
+using System.Text.Json;
+
+namespace HectreMgmt.Persistence
+{
+    public class DbInitializer
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            if (context.Chemicals.Any())
+            {
+                return; 
+            }
+            String str = "{\"Id\":\"123\",\"DateOfRegistration\":\"2012-10-21T00:00:00+05:30\",\"Status\":0}";
+
+            byte[] jsonData = Encoding.ASCII.GetBytes(@"[]");
+
+
+            var readOnlySpan = new ReadOnlySpan<byte>(jsonData);
+            var chemicals =
+                JsonSerializer.Deserialize<Chemical[]>(readOnlySpan);
+
+            context.Chemicals.AddRange(chemicals);
+            context.SaveChanges();
+        }
+
+        
+
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,6 +2,7 @@ using HectreMgmt.Domain.Model;
 using HectreMgmt.Domain.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
+using HectreMgmt.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +17,7 @@ builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseInMemoryDatabase("HectreMgmtInMemDB"));
 
 builder.Services.AddScoped<ChemicalRepo>();
-
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 
 var app = builder.Build();
@@ -26,8 +27,19 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+     DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();
